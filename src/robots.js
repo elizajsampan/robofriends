901faedc93f44b6cb8e921a6d4edcea4c03ea6b4
Eliza Jaropojop <elizajsampan0319@gmail.com>
2021-02-24 18:40:12 +0800

    Adding other files

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Erika Sampan',
+      username: 'tita rek',
+      email: 'Sincere@april.biz'
+    },
+    {
+      id: 2,
+      name: 'Emira Oku',
+      username: 'mimi',
+      email: 'Shanna@melissa.tv'
+    },
+    {
+      id: 3,
+      name: 'Herrold Tugano',
+      username: 'ninong herrold',
+      email: 'Nathan@yesenia.net'
+    },
+    {
+      id: 4,
+      name: 'Eliza Jaropojop',
+      username: 'tita aiza',
+      email: 'Julianne.OConner@kory.org'
+    },
+    {
+      id: 5,
+      name: 'Edgardo Sampan',
+      username: 'lolo gay',
+      email: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+      id: 6,
+      name: 'Shiori Oku',
+      username: 'shorshor',
+      email: 'Karley_Dach@jasper.info'
+    },
+    {
+      id: 7,
+      name: 'Consorcia Jaropojop',
+      username: 'lola mama',
+      email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+      id: 8,
+      name: 'Enzo Sampan',
+      username: 'kuya miggy',
+      email: 'Sherwood@rosamond.me'
+    },
+    {
+      id: 9,
+      name: 'Miyuki Oku',
+      username: 'yukyuk',
+      email: 'Chaim_McDermott@dana.io'
+    },
+    {
+      id: 10,
+      name: 'Mia Teshima',
+      username: 'maymay',
+      email: 'Rey.Padberg@karina.biz'
+    }
+  ];
+  
